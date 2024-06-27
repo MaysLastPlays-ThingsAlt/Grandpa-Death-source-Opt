f18bee5b00fabcb277246912e59187396d6a2170
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-06-27 09:51:36 +0300

    update source/Init.hx

--- source/Init.hx
+++ source/Init.hx
@@ -225,6 +225,9 @@ class Init extends FlxState
 
 	override public function create():Void
 	{
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
 		FlxG.save.bind('foreverengine-options');
 		Highscore.load();
 
